b44f9015e18dc681166dbec1b7629c5cdc1bfcba
Ibraheem Haffejee <ST10091630@VCCONNECT.edu.za>
2023-04-13 12:27:03 +0200

    version1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtectAccess
+{
+     class PrtctTest : ProtectAcc
+    {
+        static void Main(string[] args)
+        {
+            PrtctTest p = new PrtctTest();
+
+            Console.WriteLine("Enter your Birth year: ");
+            p.bYear = int.Parse(Console.ReadLine());
+
+            p.print();
+        }
+    }
+}
